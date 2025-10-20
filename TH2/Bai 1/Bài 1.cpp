63e60d252b66f1d30607e0c3e78bc91f7e86c4c7
mytuyen83 <huynhthimytuyen2006@gmail.com>
2025-10-20 16:22:41 +0700

    Add files via upload

---
+++ TH2/Bai 1/Bài 1.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+#define FI "MATRANKE.INP"
+#define FO "MATRANKE.OUT"
+#define MAX 1000
+
+ifstream fi;
+ofstream fo;
+
+int n;
+int A[MAX][MAX];
+int bac[MAX];
+
+void Nhap() {
+    fi.open(FI); // mở file 
+    if (!fi) {
+        cout << "Khong mo duoc file!\n";
+        return;
+    }
+    fi >> n; // đọc số đỉnh
+    for (int i = 0; i < n; i++) { // đọc ma trận
+        for (int j = 0; j < n; j++) {
+            fi >> A[i][j];
+        }
+    }
+    fi.close();
+}
+
+void XuLy() {
+    for (int i = 0; i < n; i++) { //tính bậc của đỉnh
+        bac[i] = 0;
+        for (int j = 0; j < n; j++) {
+            if (A[i][j] == 1) bac[i]++;
+        }
+    }
+}
+
+void Xuat() {
+    fo.open(FO); //xuất kq
+    if (!fo) {
+        cout << "Khong tao duoc file!\n";
+        return;
+    }
+    fo << n << endl;
+    for (int i = 0; i < n; i++) {
+        fo << bac[i] << " \n";
+    }
+    fo.close();
+}
+
+int main() {
+    Nhap();
+    XuLy();
+    Xuat();
+    return 0;
+}
